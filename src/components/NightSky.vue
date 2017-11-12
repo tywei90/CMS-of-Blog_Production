7b75cf3fa115e8c57e0913cb0b4365298557b499
魏天尧 <tywei90@gmail.com>
2017-11-12 15:00:57 +0800

    解决登录页到注册页，页面底部没有背景的bug

--- src/components/NightSky.vue
+++ src/components/NightSky.vue
@@ -9,14 +9,7 @@
 	import $  from '../js/jquery.min'
     export default{
     	ready(){
-    		// 修改页面超过一屏底部白色的bug
-    		var h1 = $(document).height();
-            var h2 = $(window).height();
-            alert(h1);
-            alert(h2);
-            setTimeout(()=>{
-                $('.nightSky').css('bottom', (h2 - h1)+'px');
-            }, 0)
+    		
     	}
     }
 </script>
